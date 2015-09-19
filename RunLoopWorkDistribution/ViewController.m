9451b5c54bb7d32e10418f5eeb6871f70f39ba9f
Di Wu <diwufet@163.com>
2015-09-19 17:13:24 +0800

    add an example uitable view

--- RunLoopWorkDistribution/ViewController.m
+++ RunLoopWorkDistribution/ViewController.m
@@ -8,15 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *IDENTIFIER = @"IDENTIFIER";
+
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *exampleTableView;
 
 @end
 
 @implementation ViewController
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 99;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:IDENTIFIER];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 80;
+}
+
+
+- (void)loadView {
+    self.view = [UIView new];
+    self.exampleTableView = [UITableView new];
+    self.exampleTableView.delegate = self;
+    self.exampleTableView.dataSource = self;
+    [self.view addSubview:self.exampleTableView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.exampleTableView.frame = self.view.bounds;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.exampleTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:IDENTIFIER];
 }
 
 - (void)didReceiveMemoryWarning {
